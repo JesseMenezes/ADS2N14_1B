235963e603cad5c96fd8642afc32d42982d17eb8
JesseMenezes <aluno@SALA201-32.FACAD.BR>
2014-05-08 20:59:25 -0200

    tentativa de teste 1

---
+++ DesertTruck/src/com/senac/dt/DesertTruck.java
@@ -0,0 +1,14 @@
+package com.senac.dt;
+
+public class DesertTruck {
+
+	public String avancar(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+
+
+}
---
+++ DesertTruck/src/com/senac/dt/dtteste.java
@@ -0,0 +1,20 @@
+package com.senac.dt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dtteste {
+	
+	@Test
+	
+	public void deveriaavancar() throws Exception{
+	DesertTruck dt = new DesertTruck();
+	String comand = dt.avancar("avancar");		
+	//assertEquals("avancar", dt.isvalidacomando(comand));
+	
+	}
+
+}
+
+
